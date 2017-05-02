abfab0659203dded27f8cd4dab6e170c29117395
robert-marriott <robertmarriot@c02s222cfvh8.users.bah.com>
2017-05-02 11:14:44 -0400

    begin player functionality

---
+++ index.js
@@ -0,0 +1,13 @@
+// ================================================================== //
+// This is the base of the shadow shroud project. It runs a NodeJS    //
+// backend that listens for input, and plays selected MP3's upon      //
+// a button trigger. It will also modulate LED's.                     //
+// ================================================================== //
+
+var player = require('player');
+var _      = require('lodash');
+
+function playSong(songName){
+console.log("variable working");
+
+}
