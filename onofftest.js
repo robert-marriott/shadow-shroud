6b0552737be5443c455171d3ce3a87073fe37f13
Robert Marriott <marriott_robert@bah.com>
2017-06-08 22:50:10 -0400

    adding onoff library button test

---
+++ onofftest.js
@@ -0,0 +1,29 @@
+var Gpio = require('onoff').Gpio;
+
+var btns[0] = new Gpio(24, 'in', 'falling', { persistentWatch: true, debounceTimeout: 300 });
+var btns[1] = new Gpio(3,  'in', 'falling', { persistentWatch: true, debounceTimeout: 300 });
+var btns[2] = new Gpio(4,  'in', 'falling', { persistentWatch: true, debounceTimeout: 300 });
+
+btns[0].watch(function (err, value) {
+    if (err) {
+        throw err;
+    } else {
+      console.log("Button 1 pressed");
+    }
+});
+
+btns[1].watch(function (err, value) {
+    if (err) {
+        throw err;
+    } else {
+      console.log("Button 2 pressed");
+    }
+});
+
+btns[2].watch(function (err, value) {
+    if (err) {
+        throw err;
+    } else {
+      console.log("Button 3 pressed");
+    }
+});
